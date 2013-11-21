adf2f7bab0f78766fb6bf309230d400038b51001
jiayiliu <jiayi.uiuc@gmail.com>
2013-11-21 13:06:09 -0500

    add bash_profile

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+# Jiayi Liu @ Nov 21, 2013
 
 ## shell script for setup purpose
 #  for first time usage on a new machine
 #  !it will remove the old file!!
 
+## copy over dot files
+cp dotfiles/bash_profile ~/.bash_profile
+cp dotfiles/bashrc ~/.bashrc
 
 
 ## platform specified setup
